bc23f721d41d955ab22f3ee860dd19f327facdec
Kevin Luo <xfalcons@yahoo.com>
2012-10-13 12:13:03 +0800

    change font

--- plugins/category_list.rb
+++ plugins/category_list.rb
@@ -23,13 +23,16 @@ module Jekyll
       html = ''
       lists.each do | category, counter |
         url = get_category_url category
-        style = "font-size: #{100 + (60 * Float(counter)/max)}%"
+        style = "font-size: #{100 + (50 * Float(counter)/max)}%"
         if @config['category_counter']
           html << " <a href='#{url}' style='#{style}'>#{category.capitalize}(#{@categories[category].count})</a> "
         else
           html << " <a href='#{url}' style='#{style}'>#{category.capitalize}</a> "
         end
       end
+
+      # testing data, should be removed
+      # html << '<a href="/category/actionscript" style="font-size: 138.9189189189189%">ActionScript(24)</a>  <a href="/category/asp" style="font-size: 103.24324324324324%">ASP(2)</a>  <a href="/category/aws" style="font-size: 101.62162162162163%">AWS(1)</a>  <a href="/category/book" style="font-size: 101.62162162162163%">Book(1)</a>  <a href="/category/browser" style="font-size: 103.24324324324324%">Browser(2)</a>  <a href="/category/cocos2d" style="font-size: 101.62162162162163%">Cocos2D(1)</a>  <a href="/category/coffeescript" style="font-size: 125.94594594594595%">CoffeeScript(16)</a>  <a href="/category/css" style="font-size: 101.62162162162163%">CSS(1)</a>  <a href="/category/diary" style="font-size: 142.16216216216216%">Diary(26)</a>  <a href="/category/django" style="font-size: 122.70270270270271%">Django(14)</a>  <a href="/category/dojo" style="font-size: 101.62162162162163%">Dojo(1)</a>  <a href="/category/easeljs" style="font-size: 104.86486486486487%">EaselJS(3)</a>  <a href="/category/flash" style="font-size: 160.0%">Flash(37)</a>  <a href="/category/funny" style="font-size: 106.48648648648648%">Funny(4)</a>  <a href="/category/happy-ruby" style="font-size: 103.24324324324324%">Happy Ruby(2)</a>  <a href="/category/html5" style="font-size: 104.86486486486487%">HTML5(3)</a>  <a href="/category/ios-app-development" style="font-size: 114.5945945945946%">iOS App Development(9)</a>  <a href="/category/javascript" style="font-size: 140.54054054054055%">JavaScript(25)</a>  <a href="/category/jquery" style="font-size: 101.62162162162163%">jQuery(1)</a>'
 
       File.open(File.join(@source, '_includes/asides/categories_tag.html'), 'w') do |file|
         file << """{% if site.category_tag_cloud %}
